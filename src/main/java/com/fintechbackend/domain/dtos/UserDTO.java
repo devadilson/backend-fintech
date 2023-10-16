4478034b0bf1e324e3590ea798541392a48f8908
Adilson <didasoueudida@gmail.com>
2023-10-16 15:29:10 -0300

    feat: create UserDTO

---
+++ src/main/java/com/fintechbackend/domain/dtos/UserDTO.java
@@ -0,0 +1,10 @@
+package com.fintechbackend.domain.dtos;
+
+import java.math.BigDecimal;
+
+import com.fintechbackend.domain.user.UserType;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email,
+        String password, UserType userType) {
+
+}
