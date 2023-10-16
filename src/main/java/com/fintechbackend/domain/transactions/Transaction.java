7e8422fb64bc3aa0e65472fc5fb8bcca66342518
Adilson <didasoueudida@gmail.com>
2023-10-16 15:27:39 -0300

    fix: rename Transaction

---
+++ src/main/java/com/fintechbackend/domain/transactions/Transaction.java
@@ -0,0 +1,48 @@
+package com.fintechbackend.domain.transactions;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fintechbackend.domain.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private BigDecimal amount;
+
+  @ManyToOne
+  @JoinColumn(name = "sender_id")
+  private User sender;
+
+  @ManyToOne
+  @JoinColumn(name = "receiver_id")
+  private User receiver;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
