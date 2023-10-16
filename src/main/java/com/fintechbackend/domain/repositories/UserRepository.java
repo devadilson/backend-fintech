7a6a98792ecbef09f0abf33b36a64186208a1c67
Adilson <didasoueudida@gmail.com>
2023-10-16 13:30:49 -0300

    feat: create UserRepository

---
+++ src/main/java/com/fintechbackend/domain/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.fintechbackend.domain.repositories;
+
+import com.fintechbackend.domain.user.User;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findUserByDocument(String document);
+
+  Optional<User> findById(Long id);
+}
