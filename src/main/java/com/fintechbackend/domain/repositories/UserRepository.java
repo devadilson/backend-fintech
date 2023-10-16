6b683fcb6f1ba36f6a3257377f3d91716dfc6dc2
Adilson <didasoueudida@gmail.com>
2023-10-16 15:26:39 -0300

    refactory: UserRepository rename findUserByid

--- src/main/java/com/fintechbackend/domain/repositories/UserRepository.java
+++ src/main/java/com/fintechbackend/domain/repositories/UserRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findUserByDocument(String document);
 
-  Optional<User> findById(Long id);
+  Optional<User> findUserById(Long id);
 }
